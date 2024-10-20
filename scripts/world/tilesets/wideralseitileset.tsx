0485f1b8a326f8a555c02225ad687d7760ef6603
jmxamongusmodder <160080970+chicken-nugget1104@users.noreply.github.com>
2024-10-20 14:36:57 -0400

    memes
    tilesetz

---
+++ scripts/world/tilesets/wideralseitileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="wideralseitileset" tilewidth="40" tileheight="40" tilecount="208" columns="16">
+ <image source="../../../assets/sprites/tilesets/wideralseitileset.png" width="667" height="552"/>
+</tileset>
